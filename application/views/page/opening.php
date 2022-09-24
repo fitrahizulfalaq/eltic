8779e62a717dfa841153433258b9496e56458db5
Fitrah Izul Falaq <fitrahizulfalaq@gmail.com>
2022-09-24 15:40:13 +0700

    opening admin no top up

--- application/views/page/opening.php
+++ application/views/page/opening.php
@@ -12,20 +12,22 @@
 		<h1>Selamat Datang,</h1>
 		<h2><?= $this->session->nama ?></h2>
 		<?php if ($this->session->tipe_user == "1"  ) { ?>
-		<h4>Poin Anda : <?= $this->fungsi->getSaldo($this->session->id) ?></h4>        
+			<h4>Poin Anda : <?= $this->fungsi->getSaldo($this->session->id) ?></h4>        
 		<?php } ?>
 		<div class="text mb-5">
 			Selamat datang di Aplikasi ELTIC. <br> Anda bisa mempelajari banyak hal melalui beragam media.
 		</div>
-
+		
 		<div class="fixed-footer">
 			<div class="row">
 				<div class="col-6">
 					<a href="<?=base_url()?>" class="btn btn-primary btn-lg btn-block"><ion-icon name="home-outline"></ion-icon> Beranda</a>
 				</div>
+				<?php if ($this->session->tipe_user == "2"  ) { ?>
 				<div class="col-6">
 					<a href="<?=base_url("topup")?>" class="btn btn-success btn-lg btn-block"><ion-icon name="cash-outline"></ion-icon> Beli Koin</a>
 				</div>
+				<?php } ?>
 			</div>
 		</div>
 	</div>
